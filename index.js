9bdd6995b04cb0e7b8054189db2b239cbf75248a
urbanovy4 <urbanovy4@users.noreply.github.com>
2022-02-09 17:17:04 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const {fromEvent, Subject, BehaviorSubject} = rxjs;
+const {takeUntil} = rxjs.operators;
+
+const documentReadyState$ = new BehaviorSubject(document.readyState);
+const onUnsubscribe$ = new Subject();
+
+fromEvent(document, 'readystatechange')
+    .pipe(
+        takeUntil(onUnsubscribe$)
+    )
+    .subscribe(event => {
+        documentReadyState$.next(event.target.readyState)
+        if (event.target.readyState === 'complete') {
+            markForUnsubscribe();
+        }
+    });
+
+documentReadyState$
+    .asObservable()
+    .pipe(takeUntil(onUnsubscribe$))
+    .subscribe(state => {
+        checkLoadingState(state);
+    });
+
+
+function markForUnsubscribe() {
+    if (documentReadyState$.getValue() === 'complete') {
+        onUnsubscribe$.next();
+        onUnsubscribe$.complete();
+        documentReadyState$.complete();
+    }
+}
+
+function checkLoadingState(state) {
+    switch (state) {
+        case 'loading':
+            onLoading();
+            break;
+        case 'interactive':
+            onInteractive();
+            break;
+        case 'complete':
+            onCompleted();
+            break;
+    }
+}
+
+function onLoading() {
+    console.log('Loading');
+}
+
+function onInteractive() {
+    console.log('Interactive');
+}
+
+function onCompleted() {
+    console.log('Completed');
+}
